983fa933310a0a9131afa353b626220f4eabae46
Polina Arakcheeva <polinaarakcheeva64@gmail.com>
2024-05-20 05:35:21 +0000

    add_all_api

--- api.py
+++ api.py
@@ -34,7 +34,7 @@ class Habr_resource(Resource):
         conn.close()
         return jsonify(ans)
 
-class date_resource(Resource):
+class dates_resource(Resource):
     def get(self, date_after, date_before, max_lim):
         try:
     # пытаемся подключиться к базе данных
@@ -60,12 +60,83 @@ class date_resource(Resource):
             loc.update({'tags':i[3]})
             loc.update({'auth_name':i[4]})
             list.append(loc)
-        res.update({'list':list})    
+        res.update({'list':list}) 
+        conn.close()   
         return res
+    
+class one_date_resource(Resource):
+    def get(self, date, max_lim):
+        try:
+# пытаемся подключиться к базе данных
+            conn = psycopg2.connect(dbname='test', user='server', password='server', host='localhost', port='5432')
+        except:
+# в случае сбоя подключения будет выведено сообщение в STDOUT
+            print('Can`t establish connection to database')
+            conn.close()
+        query =   'SELECT Articles.art_id, title, publication_date, tags, name FROM Articles JOIN Auth_art_id ON Auth_art_id.art_id = articles.art_id Join Authors ON Auth_art_id.auth_id = Authors.auth_id WHERE (date(Articles.publication_date) = '+ 'date($$'+date+'$$)'+')' +'LIMIT '+ str(max_lim)+';'
+        cur = conn.cursor()
+        cur.execute(query)
+        q_m = cur.fetchall()
+        res = {}
+        list = []
+        res.update({'count':len(q_m)})
+
+        for i in q_m :
+            loc={}
+            loc.update({'art_id':i[0]})
+            loc.update({'title':i[1]})
+            loc.update({'publication_date': str(i[2])})
+            loc.update({'tags':i[3]})
+            loc.update({'auth_name':i[4]})
+            list.append(loc)
+        res.update({'list':list})
+        conn.close()    
+        return res 
+
+class authors_resource(Resource):
+    def get(self, author_name):
+        if author_name[-1] != ' ':
+            author_name= author_name + ' '
+        try:
+# пытаемся подключиться к базе данных
+            conn = psycopg2.connect(dbname='test', user='server', password='server', host='localhost', port='5432')
+        except:
+# в случае сбоя подключения будет выведено сообщение в STDOUT
+            print('Can`t establish connection to database')
+            conn.close()   
+        cur = conn.cursor()    
+        query_a = 'SELECT auth_id, name, registration_date FROM Authors WHERE name = '+'$$'+author_name+'$$;'
+        res = {}
+        cur.execute(query_a)
+        qf_m = cur.fetchone()
+        res.update({'auth_id':qf_m[0]})
+        res.update({'name':qf_m[1]})
+        res.update({'registration_date':str(qf_m[2])})
+        query = 'SELECT Articles.art_id, title, publication_date, tags FROM Articles JOIN Auth_art_id ON Auth_art_id.art_id = articles.art_id Join Authors ON Auth_art_id.auth_id = Authors.auth_id WHERE Authors.name = '+'$$'+author_name+'$$' ';'
+        list = []
+        cur.execute(query)
+        q_m = cur.fetchall()
+        res.update({'count':len(q_m)})
+        for i in q_m :
+            loc={}
+            loc.update({'art_id':i[0]})
+            loc.update({'title':i[1]})
+            loc.update({'publication_date': str(i[2])})
+            loc.update({'tags':i[3]})
+            list.append(loc)
+        res.update({'list':list})
+        conn.close()
+        return res    
+
+
+
+            
 app = Flask(__name__)
 api = Api(app)
 api.add_resource(Habr_resource, "/articles/<int:id>")
-api.add_resource(date_resource,"/articles/<string:date_after>/<string:date_before>/<int:max_lim>")
+api.add_resource(dates_resource,"/articles/<string:date_after>/<string:date_before>/<int:max_lim>")
+api.add_resource(one_date_resource, "/articles/<string:date>/<int:max_lim>")
+api.add_resource(authors_resource, "/authors/<string:author_name>")
 app.run(debug=True)
 
 
--- request.py
+++ request.py
@@ -1,4 +1,4 @@
 import requests
 
-res = requests.get('http://127.0.0.1:5000/articles/2024-05-18T19:11:35.000Z/2024-05-20T20:11:35.000Z/3')
+res = requests.get('http://127.0.0.1:5000/authors/tmplts')
 print(res.json())
